cbd7b8b54db8a55204c2a7dc13ca07c355fe6aef
BrunoCalassi <bruno.calassi@gmail.com>
2023-05-19 11:30:13 -0300

    subject

--- send_email.py
+++ send_email.py
@@ -6,10 +6,10 @@ smtp_server = 'smtp.gmail.com'
 USERNAME = os.environ.get('USER_EMAIL')
 PASSWORD = os.environ.get('USER_PASSWORD')
 
-message = """"\
-    Subject: Teste do github actions
+message = """\
+Subject: Teste do github actions
 
-    report em forma de email teste do github actions
+report em forma de email teste do github actions
 """
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server,port,context=context) as server:
